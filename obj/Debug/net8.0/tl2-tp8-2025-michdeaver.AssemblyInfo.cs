e3acdb76ea72d83401de1b79e9daf302ad64eaae
michdeaver <michellecamilledeaver@gmail.com>
2025-10-31 18:42:01 -0300

    controller presupuestos

--- obj/Debug/net8.0/tl2-tp8-2025-michdeaver.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp8-2025-michdeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp8-2025-michdeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3090a8bc78f94edfdace18e393fea82e7fa08e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp8-2025-michdeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp8-2025-michdeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
